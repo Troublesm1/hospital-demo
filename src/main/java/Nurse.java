f1cb291436d3e9c0f98c5b2630f7375d400dbb86
Troublesm1 <Jovahnn@yahoo.com>
2021-04-25 22:04:54 -0400

    Hosp Emply Class and MedDuties Interface

--- src/main/java/Nurse.java
+++ src/main/java/Nurse.java
@@ -1,25 +1,24 @@
+import hospital.HospitalEmployee;
+import hospital.MedicalDuties;
 import hospital.Patient;
 
-public class Nurse {
+public class Nurse extends HospitalEmployee implements MedicalDuties {
 
-    private String employeeNumber;
-    private String name;
     private int numOfPatients;
 
     public Nurse(String employeeNumber, String name, int numOfPatients) {
-        this.employeeNumber = employeeNumber;
-        this.name = name;
+        super(employeeNumber, name);
         this.numOfPatients = numOfPatients;
     }
-
+    @Override
     public void careForPatient(Patient sickPatient) {
         sickPatient.increaseHealthLevel(5);
     }
-
+    @Override
     public void drawBlood(Patient sickPatient) {
         sickPatient.decreaseBloodLevel(5);
     }
-
+    @Override
     public int calculatePay() {
         return 80000;
     }
